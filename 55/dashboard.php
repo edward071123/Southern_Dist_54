99c4113fc9b7ef1ac8464e09bdd6ee519d294749
EdwardChung <leonardo071123@gmail.com>
2025-02-26 14:36:22 +0800

    update: 美化dashboard

--- 55/dashboard.php
+++ 55/dashboard.php
@@ -30,14 +30,39 @@ if (!isset($_SESSION["admin"])) {
             </div>
             <div class="col-md-8 py-3 h4">臺灣人工智慧公會(TWAIA, Taiwan Artificial Intelligence Association)</div>
             <div class="col-md-2 py-3">
-                <a href="logout.php" class="btn btn-success btn-sm">登出</a>
+                <a href="companies.php" class="btn btn-primary btn-sm">管理會員公司</a>
+                <a href="products.php" class="btn btn-info btn-sm">管理會員產品</a>
+                <a href="logout.php" class="btn btn-danger btn-sm">登出</a>
             </div>
         </div>
 
 
         <!-- 第三列: 內容區 -->
-        <div class="row bg-light text-center">
-            dashboard
+        <div class="row bg-light text-center py-5">
+            <div class="col-12">
+                <h2 class="text-primary">歡迎來到管理系統</h2>
+                <p class="lead">在這裡，您可以管理會員公司和其產品資訊，確保數據的準確性與即時性。</p>
+                <div class="row justify-content-center">
+                    <div class="col-md-4">
+                        <div class="card shadow-sm">
+                            <div class="card-body">
+                                <h5 class="card-title">管理會員公司</h5>
+                                <p class="card-text">查看、編輯和管理所有會員公司資訊。</p>
+                                <a href="companies.php" class="btn btn-primary">前往管理</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card shadow-sm">
+                            <div class="card-body">
+                                <h5 class="card-title">管理會員產品</h5>
+                                <p class="card-text">查看、編輯和管理公司相關產品資訊。</p>
+                                <a href="products.php" class="btn btn-info">前往管理</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!-- 第四列: 頁尾 -->
